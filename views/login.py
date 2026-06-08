bb2193080f355d87e71c46ec010b33d1432222b3
renzo-lorenzo <20211532@aloe.ulima.edu.pe>
2026-06-08 14:21:27 -0500

    Ajustes en el login

--- views/login.py
+++ views/login.py
@@ -11,12 +11,11 @@ def pantalla_login():
     col1, col2, col3 = st.columns([1, 1.3, 1])
 
     with col2:
-        st.markdown('<div class="card">', unsafe_allow_html=True)
 
-        st.subheader("Inicio de sesión del paciente")
+        st.subheader("Inicio de sesión")
         st.write("Ingresa tus datos para iniciar tu rutina guiada.")
 
-        nombre = st.text_input("Nombre del paciente")
+        nombre = st.text_input("Nombre")
         edad = st.number_input("Edad", min_value=18, max_value=100, value=45)
         dolor = st.slider("Nivel de dolor actual en la rodilla", 0, 10, 3)
         experiencia = st.selectbox(
@@ -38,5 +37,3 @@ def pantalla_login():
                 st.session_state.puntos = puntos_iniciales_por_experiencia(experiencia)
                 st.session_state.nivel = 1
                 st.rerun()
-
-        st.markdown('</div>', unsafe_allow_html=True)
